851c5cc07f13263a0a1f6ed50352e96c669936e4
erlint1212 <e.t.nordtvedt@proton.me>
2026-09-15 17:06:46 +0200

    added middleware to always have X-Content-Type-Options: nosniff

--- internal/httpserver/app.go
+++ internal/httpserver/app.go
@@ -237,6 +237,7 @@ func New(database *sql.DB, logger *logging.Logger, options Options) (*Applicatio
 		mainMux,
 		cspNonce,
 		recoverPanics(logger, renderer),
+		nosniffMIME,
 	)
 	return &Application{Handler: handler, publicRoot: publicRoot}, nil
 }
