8f847d10074cb9e1b6a6917cf8d1a754416e4f7b
erlint1212 <e.t.nordtvedt@proton.me>
2026-09-15 12:30:54 +0200

    added flakes for dev enviorment

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Go development environment using nixpkgs-unstable";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        
+        # Your custom build command cleanly defined here
+        build-script = pkgs.writeShellScriptBin "build-linko" ''
+          SHA=$(git rev-parse HEAD 2>/dev/null || echo "unknown")
+          TIME=$(date -u '+%Y-%m-%dT%H:%M:%SZ')
+          # Using the full boot.dev paths you specified
+          go build -ldflags "-X boot.dev/linko/internal/build.GitSHA=$SHA -X boot.dev/linko/internal/build.BuildTime=$TIME" "$@"
+        '';
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            build-script
+            go_1_27
+            gopls # language server
+            gotools # goimports, etc.
+            go-tools # staticcheck
+            delve # debugger
+            bootdev-cli
+            docker
+          ];
+
+          shellHook = ''
+            echo "Using $(go version)"
+            echo "Tip: Use 'build-linko' instead of 'go build' to compile with version flags!"
+            
+            export GOPATH="$HOME/go"
+            export PATH="$GOPATH/bin:$PATH"
+          '';
+        };
+      }
+    );
+}
